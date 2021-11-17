63ec6a236f837436bd10d38437f1d951af83feb3
kumar-arun007 <90613514+kumar-arun007@users.noreply.github.com>
2021-11-17 04:34:02 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+require('dotenv').config()
+var redis = require("redis");
+var bluebird = require("bluebird");
+
+// Convert Redis client API to use promises, to make it usable with async/await syntax
+bluebird.promisifyAll(redis.RedisClient.prototype);
+bluebird.promisifyAll(redis.Multi.prototype);
+
+console.log(redis.RedisClient.prototype);
+async function testCache() {
+
+    // Connect to the Azure Cache for Redis over the TLS port using the key.
+    var cacheConnection = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST, {auth_pass: process.env.REDIS_KEY, tls: {servername: process.env.REDIS_HOST}});
+        
+    // Perform cache operations using the cache connection object...
+
+    // Simple PING command
+    console.log("\nCache command: PING");
+    console.log("Cache response : " + await cacheConnection.pingAsync());
+
+    // Simple get and put of integral data types into the cache
+    console.log("\nCache command: GET Message");
+    console.log("Cache response : " + await cacheConnection.getAsync("Message"));    
+
+    console.log("\nCache command: SET Message");
+    console.log("Cache response : " + await cacheConnection.setAsync("Message",
+        "Hello! The cache is working from Node.js!"));    
+
+    // Demonstrate "SET Message" executed as expected...
+    console.log("\nCache command: GET Message");
+    console.log("Cache response : " + await cacheConnection.getAsync("Message"));    
+
+    // Get the client list, useful to see if connection list is growing...
+    console.log("\nCache command: CLIENT LIST");
+    console.log("Cache response : " + await cacheConnection.clientAsync("LIST"));    
+}
+
+testCache();
+
