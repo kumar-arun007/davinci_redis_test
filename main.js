1fd3b010efab04169e1a2f165950160e63364468
kumar-arun007 <90613514+kumar-arun007@users.noreply.github.com>
2021-12-16 17:31:13 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 var http = require("http");
 
-const redis = require("redis");
-const client = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST, {auth_pass: process.env.REDIS_PASSWORD, tls: {servername: process.env.REDIS_HOST}});
-let result = "";
+// const redis = require("redis");
+// const client = redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST, {auth_pass: process.env.REDIS_PASSWORD, tls: {servername: process.env.REDIS_HOST}});
+let result = "Hello World!!!!!!!!!!!";
 
-client.set("foo_rand000000000000", "arun");
+// client.set("foo_rand000000000000", "arun");
 
-// This will return a JavaScript String
-client.get("foo_rand000000000000", function(err, reply) {
-  result = reply.toString();
-  console.log(reply.toString()); // Will print `OK`
-});
+// // This will return a JavaScript String
+// client.get("foo_rand000000000000", function(err, reply) {
+//   result = reply.toString();
+//   console.log(reply.toString()); // Will print `OK`
+// });
 
 
 http.createServer(function (request, response) {
